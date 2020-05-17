88274bd6a3aaa0841cb6429f068474a6a6fbef92
Greg Lincoln <greg@greglincoln.com>
2020-05-17 07:48:09 -0400

    Working repeater. Cleanups.

--- fw/8266RepeaterNow/src/main.cpp
+++ fw/8266RepeaterNow/src/main.cpp
@@ -1,43 +1,40 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h>
 
-// Structure example to receive data
-// Must match the sender structure
-typedef struct struct_message {
-  float temp;
-  float humidity;
-  float pressure;
-} struct_message;
-
-// Create a struct_message called myData
-struct_message myData;
-
-// Callback function that will be executed when data is received
-void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
-  memcpy(&myData, incomingData, sizeof(myData));
-  Serial.printf("Temperature: %.2f C, Pressure: %.2f Pa, Humidity: %.2f\n", 
-          myData.temp, myData.pressure, myData.humidity);
+void blink(unsigned int time, unsigned int count)
+{
+  for (unsigned int j = 0; j < count; j++)
+  {
+    digitalWrite(LED_BUILTIN_AUX, LOW);
+    delay(time / 2);
+    digitalWrite(LED_BUILTIN_AUX, HIGH);
+    delay(time / 2);
+  }
 }
- 
-void setup() {
-  // Initialize Serial Monitor
-  Serial.begin(115200);
-  Serial.print("ESP8266 Board MAC Address:  ");
-  Serial.println(WiFi.macAddress());
 
-  // Set device as a Wi-Fi Station
+void OnDataRecv(uint8_t *mac, uint8_t *incomingData, uint8_t len)
+{
+  Serial.write(incomingData, len);
+}
+
+void setup() {
+  pinMode(LED_BUILTIN_AUX, OUTPUT);
+  Serial.begin(115200);
+  //  Serial.print("ESP8266 Board MAC Address:  ");
+  //  Serial.println(WiFi.macAddress());
+
   WiFi.mode(WIFI_STA);
 
-  // Init ESP-NOW
   if (esp_now_init() != 0) {
-    Serial.println("Error initializing ESP-NOW");
+    blink(100, 3);
+    blink(500, 3);
+    blink(100, 3);
     return;
   }
   
-  // Once ESPNow is successfully Init, we will register for recv CB to
-  // get recv packer info
   esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
   esp_now_register_recv_cb(OnDataRecv);
+  blink(500, 4);
 }
 
 void loop() {
---
+++ fw/8266RepeaterWifi/src/main.cpp
@@ -0,0 +1,77 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include "../../secure.h"
+
+typedef struct test_struct
+{
+  float temp;
+  float humidity;
+  float pressure;
+} test_struct;
+
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient);
+char message[100];
+
+void blink(unsigned int time, unsigned int count)
+{
+  for (unsigned int j = 0; j < count; j++)
+  {
+    digitalWrite(LED_BUILTIN_AUX, LOW);
+    delay(time / 2);
+    digitalWrite(LED_BUILTIN_AUX, HIGH);
+    delay(time / 2);
+  }
+}
+
+void reconnect_mqtt()
+{
+  // Loop until we're reconnected
+  while (!mqttClient.connected())
+  {
+    blink(100, 1);
+    blink(200, 1);
+    blink(100, 1);
+    if (mqttClient.connect("ToonyWeather"))
+    {
+      // Subscribe or resubscribe to a topic
+    }
+    else
+    {
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup()
+{
+  pinMode(LED_BUILTIN_AUX, OUTPUT);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    blink(100, 1);
+  }
+
+  mqttClient.setServer(mqtt_server, 1883);
+  //client.setCallback(callback);
+  reconnect_mqtt();
+  blink(500, 4);
+  mqttClient.publish("test/log", "Connected!");
+  Serial.begin(115200);
+}
+
+void loop()
+{
+  if (!mqttClient.connected())
+  {
+    reconnect_mqtt();
+  }
+  if (Serial.available())
+  {
+    test_struct data;
+    Serial.readBytes((uint8_t *)&data, sizeof(test_struct));
+    //sprintf(message, "{\"temp\": %f, \"humidity\": %f, \"pressure\": %f}", data.temp, data.humidity, data.pressure);
+    mqttClient.publish("test/weather", (const uint8_t *)&data, sizeof(test_struct));
+  }
+}
